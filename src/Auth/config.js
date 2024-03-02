8c77cb2841ae5bfc6bbc8197126ad55c38fc8a91
rudrax14 <m.rudra2003@gmail.com>
2024-03-02 16:54:32 +0530

    Add Firestore functionality and update logout button

--- src/Auth/config.js
+++ src/Auth/config.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -20,4 +19,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
-export { auth, provider };
+const firestore = getFirestore(app); // Use 'app' instead of 'firebaseApp'
+export { auth, provider, firestore };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,46 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { SidebarContext } from '../contexts/SidebarContext';
-import {BsBag} from "react-icons/bs"
+import { BsBag } from "react-icons/bs"
 import { CartContext } from '../contexts/CartContext';
-import { Link, useSearchParams } from 'react-router-dom';
+import { Link, Navigate, useSearchParams } from 'react-router-dom';
 import Logo from "../img/logo.svg"
 const Header = () => {
-  const [isActive,setIsActive] = useState(true);
-  const {isOpen,setIsOpen} =  useContext(SidebarContext);
-  const {itemAmount} = useContext(CartContext);
-  useEffect(()=>{
-    window.addEventListener('scroll',()=>{
+  const [isActive, setIsActive] = useState(true);
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
+  const { itemAmount } = useContext(CartContext);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
       window.scrollY > 60 ? setIsActive(false) : setIsActive(true);
     })
   })
-  return <header className={`${isActive?'bg-white py-4 shadow-md':'bg-none py-6'} fixed w-full z-10 transition-all`}>
+  const logout = () => {
+    localStorage.clear();
+    Navigate('/');
+  }
+  return <header className={`${isActive ? 'bg-white py-4 shadow-md' : 'bg-none py-6'} fixed w-full z-10 transition-all`}>
     <div className='container flex mx-auto items-center justify-between h-full'>
-    <Link to={'/'}>
-        <div>
-          <img className='w-[40px]' src={Logo} />
+      <Link to={'/'}>
+        <div className='flex gap-6 items-center'>
+
+          <div>
+            <img className='w-[40px]' src={Logo} />
+          </div>
+          <div>
+            <button onClick={logout}>Logout</button>
+          </div>
         </div>
       </Link>
-   
-      
 
-    <div className='cursor-pointer flex relative' onClick={()=> setIsOpen(!isOpen)}>
-      <BsBag className='text-2xl' />
-      <div className='bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] 
+
+
+      <div className='cursor-pointer flex relative' onClick={() => setIsOpen(!isOpen)}>
+        <BsBag className='text-2xl' />
+        <div className='bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] 
       h-[18px] text-white rounded-full flex justify-center items-center'>{itemAmount}</div>
+      </div>
     </div>
-    </div>
-  </header>;  
-  
+  </header>;
+
 
 };
 
